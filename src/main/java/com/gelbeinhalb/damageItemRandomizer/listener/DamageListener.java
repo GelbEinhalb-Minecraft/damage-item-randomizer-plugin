e199a8a1710ca1b21d8868aa80ef0e049a1225e0
GelbEinhalb <exzitonyw@gmail.com>
2024-12-27 22:08:06 +0100

    allow hand damage

--- src/main/java/com/gelbeinhalb/damageItemRandomizer/listener/DamageListener.java
+++ src/main/java/com/gelbeinhalb/damageItemRandomizer/listener/DamageListener.java
@@ -1,10 +1,13 @@
 package com.gelbeinhalb.damageItemRandomizer.listener;
 
 import com.gelbeinhalb.damageItemRandomizer.utils.RandomItem;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,7 +22,8 @@ public class DamageListener implements Listener {
         if (!player.getLocation().getWorld().getName().equals("void_world")) {
             return;
         }
-        int itemCount = (int) (event.getDamage() / 2);
+
+        int itemCount = getItemCount(event);
         for (int i = 0; i < itemCount; i++) {
             ItemStack randomItem = new RandomItem().get();
             if (player.getInventory().firstEmpty() != -1) {
@@ -28,6 +32,19 @@ public class DamageListener implements Listener {
                 player.getWorld().dropItem(player.getLocation(), randomItem);
             }
         }
+    }
+
+    private static int getItemCount(EntityDamageEvent event) {
+        int itemCount = (int) (event.getDamage() / 2);
+        if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
+            Entity damager = ((EntityDamageByEntityEvent) event).getDamager();
+            if (damager instanceof Player attacker) {
+                if (attacker.getInventory().getItemInMainHand().getType() == Material.AIR && attacker.getInventory().getItemInOffHand().getType() == Material.AIR) {
+                    itemCount = 1;
+                }
+            }
+        }
+        return itemCount;
     }
 
 }
